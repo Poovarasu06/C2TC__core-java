aa9886409f9b49e5b9cb547e6e7496a398635ce2
kingg <kingg@POOVARASU>
2025-08-17 13:00:17 +0530

    my work on day 3

---
+++ c2tccorejava/src/day3/constructor/Encapsulation/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package day3.constructor.Encapsulation;
+
+public class EncapsulationDemo {
+public static void main(String[] args) {
+		
+	
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Poovarasu");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+}
